12eb0e53bc75671bee69200c9f437532cc7a7275
cminhho <hmchung92@gmail.com>
2015-02-05 15:26:10 +0700

    chore(example && slide): add example & edit slide/index.html

---
+++ examples/web-scoket/server.js
@@ -0,0 +1,11 @@
+﻿var nodeJsWebSocket = require("nodejs-websocket");
+var server = nodeJsWebSocket.createServer(function(connection){
+	connection.on("text", function(msg){
+		server.connections.forEach(function(con){
+			con.sendText(msg);
+			console.log(msg);
+		});
+	});
+});
+server.listen("9090");
+console.log("Socket server listening on port 9090");
